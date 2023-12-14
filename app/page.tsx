ccee9e1053480922465c22010f1b2464dd863369
Natalya <natalyasafarevich@gmail.com>
2023-12-14 21:53:21 +0100

    component how we work has been added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import styles from './page.module.scss';
 import Banner from '../components/Banner/Banner';
+import HowWeWork from '../components/HowWeWork/HowWeWork';
 
 export default function Home() {
   return (
     <main className={'styles.main '}>
       <div className={styles.page__banner}>
         <Banner />
+        <HowWeWork />
       </div>
     </main>
   );
--- constants/constants.ts
+++ constants/constants.ts
@@ -20,3 +20,26 @@ export const NAV_LINKS: Array<NavLinkT> = [
     to: 'blog ',
   },
 ];
+
+type ItemsT = {
+  img: string;
+  label: string;
+  desc: string;
+};
+export const HOW_WORK_ITEMS: Array<ItemsT> = [
+  {
+    img: '/careate-icon.svg',
+    label: 'Create an account',
+    desc: `Aspernatur sit adipisci quaerat unde Redug Lagre dolor sit amets consectetus. Agencies define their new business`,
+  },
+  {
+    img: '/bank-icon.svg',
+    label: 'Attach bank accounts',
+    desc: `Aspernatur sit adipisci quaerat unde Redug Lagre dolor sit amets consectetus. Agencies define their new business`,
+  },
+  {
+    img: '/money-icon.svg',
+    label: 'Send money',
+    desc: `Aspernatur sit adipisci quaerat unde Redug Lagre dolor sit amets consectetus. Agencies define their new business`,
+  },
+];
