3d4596eee70a5c46103b53f9748a3a9914621391
Natalya <natalyasafarevich@gmail.com>
2023-12-14 21:37:40 +0100

    banner has been added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import styles from './page.module.scss';
+import Banner from '../components/Banner/Banner';
 
 export default function Home() {
-  return <main className={'styles.main '}></main>;
+  return (
+    <main className={'styles.main '}>
+      <div className={styles.page__banner}>
+        <Banner />
+      </div>
+    </main>
+  );
 }
